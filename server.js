38d4d51c68182c1d32f324664f5fa7cf5bed66d4
luis2 <luis212urbina@gmail.com>
2019-12-04 17:13:44 -0600

    changes made

--- server.js
+++ server.js
@@ -15,15 +15,16 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : 'postgres://xnnrsaatxwjtse:6ab81cf7583d2bc1bd1192fdca40f1aad4ae14684d2f867a00f3f9a683767237@ec2-54-225-115-177.compute-1.amazonaws.com:5432/db5djgp5d97lc9',
+      connectionString : process.env.DATABASE_URL,
       ssl : true,
     }
   });
-
+ console.log(DATABASE_URL);
 // bodyParser allows us to format the responses and requests
 app.use(bodyParser.json());
 // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 app.use(cors());
+
 
 app.get('/', (req,res)=>res.json('welcome')); 
 app.post('/signin', (req, res) => { signin.handleSignIn(req,res,db,bcrypt)});
