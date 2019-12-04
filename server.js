14cd63269d61dc8a2c8119e327d899d4a8fd13dd
luis2 <luis212urbina@gmail.com>
2019-12-04 14:57:14 -0600

    adding port

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.put('/image', (req,res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req,res) => { image.handleApiCall(req, res)});
 
 // second parameter is a function that runs right after the listen
-app.listen(3000,()=>{
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT|| 300,()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
 })
