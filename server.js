378b39cfd53723cee5573281e0109ab53982b901
luis2 <luis212urbina@gmail.com>
2019-12-04 17:19:28 -0600

    whatever

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl : true,
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 // bodyParser allows us to format the responses and requests
